cf42ce5b7f2cb24fb5c53acb47f7e91274b4b887
leidy <leidyu.f@gmail.com>
2021-02-25 23:22:43 -0500

    working on relationships

--- src/assets/js/custom_models/string_diagram/StringDiagramModel.ts
+++ src/assets/js/custom_models/string_diagram/StringDiagramModel.ts
@@ -12,11 +12,43 @@ export class StringDiagramModel extends Model {
         constraints = [
             {
                 "source":"true", "type":"function", "attr":null,
-                "value":null, "min":0, "max":null, "validNeighbors":null,
-                "countError":"Only 1 target allowed", "typeError":"Only shape targets allowed"
+                "value":null, "min":0, "max":null, "validNeighbors":["function"],
+                "countError":"Invalid connection", "typeError":"Only shape targets allowed"
             },
             
         ];
         this.setConstraints(constraints);
+
+        let relationStyles = this.getRelationStyles();
+        relationStyles.push(
+            { 
+                "type":"and", 
+                "source":["function"], 
+                "target":["function"],
+            }
+        );
+        this.setRelationStyles(relationStyles);
     }
+
+    public customConstraintsRelations(graph:any, source:any, target:any){
+        let returnConstraintRelations = {};
+
+		//only one custom file per component
+		if(target.getAttribute("type") == "function"){
+			let targetId = target.getId();
+			let incoEgdes = graph.getModel().getIncomingEdges(graph.getModel().getCell(targetId));
+            console.log(incoEgdes)
+			for (let j = 0; j < incoEgdes.length; j++) {
+				if(incoEgdes[j].source.getAttribute("type")=="custom"){
+                    returnConstraintRelations = {
+                        "message":"Invalid connection only one Custom. File can be linked for this component"
+                    }
+                    break;
+				}
+			}
+		}
+
+		return returnConstraintRelations;
+	}
 }
+
